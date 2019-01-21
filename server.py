253592a0be317edaf6259d845d599cd2fd3dd63b
shelley <stian3@ualberta.ca>
2019-01-20 20:21:20 -0500

    can load pages

--- server.py
+++ server.py
@@ -26,21 +26,77 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-
+def read_path(data):
+    begin = data.find(bytes('/', 'utf-8'))
+    end =  data.find(bytearray('HTTP/1.1', 'utf-8'))
+    #print('begin = ' + str(begin) + 'end = ' + str(end))
+    return begin, end
+    
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
+        #self.data = self.request.recv(1024).strip()
+        self.data = self.request.recv(1024).decode('utf-8')
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        
+        string_list = self.data.split(' ')
+        method = string_list[0]
+        path = string_list[1].split('?')[0]
+        print("method = " + method)
+        print ("path = " + path)
 
+        # path = path.lstrip('/')
+           
+        try:            
+            if(path.endswith('.css')):
+                #css file
+                mimetype = 'text/css'
+                header = 'HTTP/1.1 200 OK\n'
+            elif(path.endswith('.html')):
+                #html file
+                mimetype = 'text/html'
+                header = 'HTTP/1.1 200 OK\n'
+            elif(path.endswith('/')): 
+                #directory, go to index
+                mimetype = 'text/html'
+                path += 'index.html'
+                header = 'HTTP/1.1 301 Permanently Moved\n'
+            else:
+                #directory, go to index
+                #redirect?
+                mimetype = 'text/html'
+                path += '/index.html'
+                # path += '/'
+                header = 'HTTP/1.1 301 Permanently Moved\n'
+            
+            header += 'Content-Type: '+str(mimetype)+'\n\n'
+
+            print('opening ./www' + path)
+            file = open('./www' + path, 'rb')
+            response = file.read()
+            file.close()
+            
+        except Exception as e:  
+            header = 'HTTP/1.1 404 Not Found\n\n'
+            response = """<html>
+                          <body>
+                            <center>
+                             <h3>Error 404: File not found</h3>
+                             <p>Python HTTP Server</p>
+                            </center>
+                          </body>
+                        </html>""".encode('utf-8')
+        
+        self.request.sendall(header.encode('utf-8')+response)
+        #self.request.close()
+        
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
-
+        
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
